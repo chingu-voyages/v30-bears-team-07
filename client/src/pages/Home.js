a8e4d9db7b010cba64310a54d5a0df6d9af61c8a
Sebastian <slf.pro@gmail.com>
2021-05-11 15:30:05 +0300

    deploy FE on netlify

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-
-
   return (
     <div>
-        <p>Home Page</p>
-        <p>Amazing App Coming Soon!</p>
-        <Link to='/'>Home Page</Link>
-        <Link to='login'>Login Page</Link>
-        <Link to='dashboard'>Dashboard Page</Link>
-        <Link to='signup'>Signup Page</Link>
+      <p>Go-on page app</p>
+      <p>Amazing App Coming Soon!</p>
+      <Link to="/">Home Page</Link>
+      <Link to="login">Login Page</Link>
+      <Link to="dashboard">Dashboard Page</Link>
+      <Link to="signup">Signup Page</Link>
     </div>
   );
 };
