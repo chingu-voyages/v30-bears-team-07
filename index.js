fc21aed8031011e82415d855c799cb886a529e0a
Sebastian <slf.pro@gmail.com>
2021-05-12 11:59:18 +0300

    Remove serving client from BE server

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
-require('dotenv').config()
-const path         = require("path")
-const express      = require('express')
-const mongoose     = require('mongoose')
-const bodyParser   = require('body-parser')
-const cors         = require('cors')
-const helmet       = require('helmet')
-const noCache      = require('nocache')
-const logger       = require('morgan')
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const noCache = require("nocache");
+const logger = require("morgan");
 
-const app = express()
-app.use(cors())
-app.use(logger('dev'))
+const app = express();
+app.use(cors());
+app.use(logger("dev"));
 // helmet security
-app.use(noCache())
-app.use(helmet())
-app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(noCache());
+app.use(helmet());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-// use eg client in front end app
-/app.use(express.static('client'));
-// Serve static files from the React frontend app for production
-app.use(express.static(path.join(__dirname, 'client/build')))
-
-// get routes 
-const authRouter   = require('./server/routes/auth')
+// get routes
+const authRouter = require("./server/routes/auth");
 //  routes
-app.use('/auth', authRouter)
+app.use("/auth", authRouter);
 
 const connect = async () => {
-
   // database connection
-  const PORT         = process.env.PORT || 5000
-  const MONGO_URL    = process.env.MONGO_URL
-  const MONGO_CONFIG =  {    
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-  }
-  
+  const PORT = process.env.PORT || 5000;
+  const MONGO_URL = process.env.MONGO_URL;
+  const MONGO_CONFIG = {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
   try {
     mongoose.Promise = global.Promise;
-    await mongoose.connect(MONGO_URL, MONGO_CONFIG)
-    console.log('Successfully connected MongoDB!')
+    await mongoose.connect(MONGO_URL, MONGO_CONFIG);
+    console.log("Successfully connected MongoDB!");
   } catch (err) {
-    console.log('Mongoose error', err);
+    console.log("Mongoose error", err);
   }
   // start express server
   app.listen(PORT);
-  console.log(`Server listening on localhost:${PORT}`)
-}
+  console.log(`Server listening on localhost:${PORT}`);
+};
 
 connect();
