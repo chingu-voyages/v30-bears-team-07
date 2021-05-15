c6455598f1ea3524164b8c3b1c7b3afacd58ca30
Rammina <altinaschwarzer@gmail.com>
2021-05-14 20:57:05 -0600

    Added .scss files for components

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -46,3 +46,40 @@ const LoginForm = ({
 };
 
 export default LoginForm;
+
+//
+//
+// Going to add this for later (tella - 5/15/21)
+/* 
+  <form id="login-form" autoComplete="off">
+      <div className="login form-content-container">
+        <div className="door-title-container">
+          <h2 className="heading">Login</h2>
+        </div>
+        {renderErrorNotifications()}
+        <div className="textfield-container">
+          
+        </div>
+        <div className="textfield-container">
+          
+        </div>
+
+        <div className="form-button-container">
+          <button
+            className={"form-button submit mt-20"}
+            type="submit"
+            onClick={props.handleSubmit(onSubmit)}
+          >
+            {renderLoader()} Sign In
+          </button>
+        </div>
+        <Link
+          id="register-text-link"
+          className="small-text-link"
+          to={`/auth/register`}
+        >
+          Don't have an account? Register here.
+        </Link>
+      </div>
+    </form>
+*/
