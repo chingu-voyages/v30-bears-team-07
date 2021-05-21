9275d97bf745c5ac8318db7649cabe970579cb31
Vlado <vladimir.kokic@gmail.com>
2021-05-21 21:26:12 +0200

    continued work on Dashboard styling
    - mobile and desktop version

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -7,29 +7,25 @@ const Dashboard = () => {
   const projects = [1, 2, 3, 4, 5];
   return (
     <div>
-      {/*<section className="header">
-        <p>Crowdfund</p>
-        <div className="header__right">
-          <p>About</p>
-          <p>Discover</p>
-          <p>Get Started</p>
-          <GoogleAuth />
-        </div>
-      </section>*/}
+      <section className="header">
+        <div className="header__right">{/*<GoogleAuth />*/}</div>
+      </section>
       <section className="main">
         <div className="main__left">
           <div className="main__left--info">
             <div>Settings</div>
             <div>Fundraiser</div>
             <div>Donations</div>
-            <div>Payment Info</div>
+            <div>Info</div>
           </div>
         </div>
         <div className="main__right">
           {projects.map((el) => (
             <div className="main__right--card">
               <div>Image: </div>
-              <div>Text: </div>
+              <h4>Let's Get Dorian and Louise Wed</h4>
+              <p>Last Donation: 8 min ago</p>
+              <p>$15,916 raised</p>
             </div>
           ))}
         </div>
