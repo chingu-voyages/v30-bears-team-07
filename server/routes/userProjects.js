1cb6c5d4f07630f7bd692e95db72875d2ab235b1
Vlado <vladimir.kokic@gmail.com>
2021-05-31 19:34:09 +0200

    fixed typo

--- server/routes/userProjects.js
+++ server/routes/userProjects.js
@@ -2,9 +2,7 @@ const express = require("express");
 // this enables the userProjects router to get the params of the parent route users
 const router = express.Router({ mergeParams: true });
 
-const {
-  get_all_user_projects,
-} = require("../controllers/projects");
+const { get_all_user_projects } = require("../controllers/projects");
 
 // get all the userProjects of the user
 router.get("/", get_all_user_projects);
