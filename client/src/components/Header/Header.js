03e7f13c8646d7ac06aa91ee01e20226e51f9d7f
Vlado <vladimir.kokic@gmail.com>
2021-06-05 21:10:45 +0200

    added hamburger menu

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import GoogleAuth from "../../components/GoogleAuth/GoogleAuth";
@@ -7,22 +7,37 @@ import "./Header.scss";
 
 function Header(props) {
   const isAuth = useSelector((state) => state.auth.isSignedIn);
+  const [open, setOpen] = useState(false);
+  const toggleHeader = open ? "header display-block" : "header display-none";
+  const toggleBurger = open ? "burger display-none" : "burger display-block";
+  const toggleIcon = open ? "icon display-block" : "icon display-none";
+  const handleClose = () => setOpen(!open);
 
   return (
-    <div className="header">
-      <section className="nav">
-        <Link to="/allprojects">All Projects</Link>
+    <div>
+      <section className={toggleBurger} onClick={handleClose}>
+        <div />
+        <div />
+        <div />
       </section>
+      <div className={toggleHeader}>
+        <span className={toggleIcon} onClick={handleClose}>
+          Minimize
+        </span>
+        <section className="nav">
+          <Link to="/allprojects">All Projects</Link>
+        </section>
 
-      <section className="nav">
-        {!isAuth ? <Link to="/login">Log in</Link> : ""}
+        <section className="nav">
+          {!isAuth ? <Link to="/login">Log in</Link> : ""}
 
-        {isAuth ? <Link to="/dashboard">Dashboard</Link> : ""}
-      </section>
+          {isAuth ? <Link to="/dashboard">Dashboard</Link> : ""}
+        </section>
 
-      <section>
-        <GoogleAuth />{" "}
-      </section>
+        <section>
+          <GoogleAuth />{" "}
+        </section>
+      </div>
     </div>
   );
 }
