278fe4e9be959e5ea12e181bcd58f1d112261626
Rammina <altinaschwarzer@gmail.com>
2021-06-12 23:20:11 -0600

    Dispatching clearError when page unrenders

--- client/src/components/forms/project/DonateForm/DonateForm.js
+++ client/src/components/forms/project/DonateForm/DonateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { Field, reduxForm } from "redux-form";
 import { useSelector, useDispatch } from "react-redux";
