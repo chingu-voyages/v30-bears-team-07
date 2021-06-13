b0d7bd33bebacf6d8678b99bf835433924896a4e
Rammina <altinaschwarzer@gmail.com>
2021-06-12 22:42:12 -0600

    Dispatching clearError when page unrenders

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { clearErrors } from "../../redux/actions/errorActions";
 import LoginForm from "../../components/forms/auth/LoginForm/LoginForm";
 import GoogleAuth from "../../components/GoogleAuth/GoogleAuth";
 
 const Login = ({}) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearErrors());
+    };
+  }, []);
+
   return (
     <main className="auth page-container">
       <section className="auth__form-container">
